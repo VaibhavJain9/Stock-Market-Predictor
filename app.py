379b79377b01e218d938e838f8d3008d168de329
VaibhavJain9 <96428631+VaibhavJain9@users.noreply.github.com>
2023-08-08 00:27:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ import pandas as pd
 from sklearn.preprocessing import StandardScaler
 from tensorflow.keras.layers import Input, LSTM, Dense
 from tensorflow.keras.models import Model
+from tensorflow.keras.models import Sequential
 import matplotlib
 matplotlib.use('Agg')
 from flask import render_template, request,jsonify
@@ -45,103 +46,81 @@ def generate_chart():
     start_date = request.form['start_date']
     end_date = request.form['end_date']
 
-    stockprices = yf.download(symbol, start=start_date, end=end_date)
+    stock_prices = yf.download(symbol, start=start_date, end=end_date)
+    stock_prices_df = pd.DataFrame(stock_prices)
 
-    test_ratio = 0.2
-    training_ratio = 1 - test_ratio
-
-    # Data Splitting
-    train_size = int(training_ratio * len(stockprices))
-    train = stockprices[:train_size][["Close"]]
-    test = stockprices[train_size:][["Close"]]
-
-    def extract_seqX_outcomeY(data, N, offset):
-        X, y = [], []
-        for i in range(offset, len(data)):
-            X.append(data[i - N: i])
-            y.append(data[i])
-        return np.array(X), np.array(y)
-
-    def calculate_rmse(y_true, y_pred):
-        rmse = np.sqrt(np.mean((y_true - y_pred) ** 2))
-        return rmse
-
-    def preprocess_test_data(data=stockprices, scaler=None, window_size=None, test=None):
-        raw = data["Close"][len(data) - len(test) - window_size:].values
-        raw = raw.reshape(-1, 1)
-        raw = scaler.transform(raw)
-
-        X_test = [raw[i - window_size: i, 0] for i in range(window_size, raw.shape[0])]
-        X_test = np.array(X_test)
-
-        X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
-        return X_test
-
-    # Scaling the data
+    
+    cols = list(stock_prices_df)[1:7]
+    
+    df_for_training = stock_prices_df[cols].astype(float)
+    
     scaler = StandardScaler()
-    scaled_data = scaler.fit_transform(stockprices[["Close"]])
-    scaled_data_train = scaled_data[:train_size]
+    scaler = scaler.fit(df_for_training)
+    df_for_training_scaled = scaler.transform(df_for_training)
+    
+    trainX = []
+    trainY = []
+    
+    n_future = 1  
+    n_past = 14  
+    for i in range(n_past, len(df_for_training_scaled) - n_future +1):
+     trainX.append(df_for_training_scaled[i - n_past:i, 0:df_for_training.shape[1]])
+     trainY.append(df_for_training_scaled[i + n_future - 1:i + n_future, 0])
 
-    # Preparing the LSTM model
-    window_size = 50
-    X_train, y_train = extract_seqX_outcomeY(scaled_data_train, window_size, window_size)
+    trainX, trainY = np.array(trainX), np.array(trainY) 
+    
+    model = Sequential()
+    model.add(LSTM(64, activation='relu', input_shape=(trainX.shape[1], trainX.shape[2]), return_sequences=True))
+    model.add(LSTM(32, activation='relu', return_sequences=False))
+    
+    model.add(Dense(trainY.shape[1]))
+    
+    model.compile(optimizer='adam', loss='mse')
+    model.summary()
+    
+    history = model.fit(trainX, trainY, epochs=5, batch_size=16, validation_split=0.1, verbose=1)
+    
+    n_past = 16
+    n_days_for_prediction = 30
+    
+    predict_period_dates = pd.date_range(list(stock_prices_df.index)[-n_past], periods=n_days_for_prediction, freq='B').tolist()
+    
+    prediction = model.predict(trainX[-n_days_for_prediction:])
+    prediction_copies = np.repeat(prediction, df_for_training.shape[1], axis=-1)
+    y_pred_future = scaler.inverse_transform(prediction_copies)[:, 0]
+    
+    forecast_dates = []
+    for time_i in predict_period_dates:
+        forecast_dates.append(time_i.date())
+    
+    df_forecast = pd.DataFrame({'Date': np.array(forecast_dates), 'Open': y_pred_future})
+    df_forecast['Date'] = pd.to_datetime(df_forecast['Date'])
+    
+    # Filter df_forecast to include only rows with dates greater than or equal to end_date
+    df_forecast_filtered = df_forecast[df_forecast['Date'] >= end_date]
+    
+    fig, ax = plt.subplots()
+    
+   
+    ax.plot(stock_prices_df.index, stock_prices_df['Open'], label='Original')
+    ax.plot(df_forecast_filtered['Date'], df_forecast_filtered['Open'], label='Predicted')
+    
+    ax.set_xlabel('Date')
+    ax.set_ylabel('Open Price')
+    ax.set_title('Stock Price Prediction')
+    ax.legend()
+    plt.xticks(rotation=45)
+    plt.show()
 
-    def run_lstm_model(X_train, layer_units=10):
-        inp = Input(shape=(X_train.shape[1], 1))
-        x = LSTM(units=layer_units, return_sequences=True)(inp)
-        x = LSTM(units=layer_units)(x)
-        out = Dense(1, activation="linear")(x)
-        model = Model(inp, out)
-        model.compile(loss="mean_squared_error", optimizer="adam")
-        return model
-
-    model = run_lstm_model(X_train, layer_units=10)
-
-    # Train the LSTM model
-    cur_epochs = 5
-    cur_batch_size = 100
-
-    history = model.fit(
-        X_train,
-        y_train,
-        epochs=cur_epochs,
-        batch_size=cur_batch_size,
-        verbose=1,
-        validation_split=0.1,
-        shuffle=True,
-    )
-
-    # Preprocess test data
-    X_test = preprocess_test_data(data=stockprices, scaler=scaler, window_size=window_size, test=test)
-
-    # Make predictions using the LSTM model
-    predicted_price_ = model.predict(X_test)
-    predicted_price = scaler.inverse_transform(predicted_price_)
-
-    # Add the predicted prices to the test dataframe
-    test["Predictions_lstm"] = predicted_price
-
-    # Evaluate performance
-    rmse_lstm = calculate_rmse(np.array(test["Close"]), np.array(test["Predictions_lstm"]))
-    print("Root Mean Squared Error (RMSE) of LSTM model:", rmse_lstm)
-
-    def plot_stock_trend_lstm(train, test):
-        fig = plt.figure(figsize=(20, 10))
-        plt.plot(train.index, train["Close"], label="Train Closing Price")
-        plt.plot(test.index, test["Close"], label="Test Closing Price")
-        plt.plot(test.index, test["Predictions_lstm"], label="Predicted Closing Price")
-        plt.title(f"LSTM Model - {symbol}")
-        plt.xlabel("Date")
-        plt.ylabel("Stock Price ($)")
-        plt.legend(loc="upper left")
-
-        # Save the plot to a BytesIO object
-        output = io.BytesIO()
-        FigureCanvas(fig).print_png(output)
-        return Response(output.getvalue(), mimetype='image/png')
-
+    
+    # Save the plot to a BytesIO object
+    output = io.BytesIO()
+    FigureCanvas(fig).print_png(output)
+    return Response(output.getvalue(), mimetype='image/png')
+    
     # Return the plot using the defined function
     return plot_stock_trend_lstm(train, test)
+    
 
 if __name__ == '__main__':
     app.run(debug=True, port=5004)
